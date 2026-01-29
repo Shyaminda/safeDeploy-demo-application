5caa7605fdb9ab94d01d59dc4b17f2f8166967ba
Nipun Malinga <nipunmalinga11@gmail.com>
2026-01-29 12:46:03 +0530

    feature: add basic testing

--- index.ts
+++ index.ts
@@ -1,30 +1,41 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "ok - Application reached" });
+let count = 0;
+
+app.get('/health', (_req, res) => {
+  res.status(200).json({ status: 'ok - Application reached' });
 });
 
-app.get("/slow", async (_req, res) => {
+app.get('/slow', async (_req, res) => {
   const delayMs = Number(3000);
 
   await new Promise((resolve) => setTimeout(resolve, delayMs));
 
   res.status(200).json({
-    status: "slow",
+    status: 'slow',
     delayMs,
   });
 });
 
-app.get("/error", (_req, res) => {
+app.get('/error', (_req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "Intentional error for SafeDeploy demo",
+    status: 'error',
+    message: 'Intentional error for SafeDeploy demo',
+  });
+});
+
+app.get('/sum', (_req, res) => {
+  res.status(200).json({
+    status: 'sum',
+    sum: ++count,
   });
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export {app, count};
---
+++ tests/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app, count } from '../index.js';
+
+describe('GET /sum', () => {
+  it('should return incremental value', async () => {
+    const res = await request(app).get('/sum');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      status: 'sum',
+      sum: count,
+    });
+  });
+});
