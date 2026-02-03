97d1230ae0394431465486912019f3aea1e0425f
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-02-03 20:03:21 +0530

    fix: remove slow mode condition from health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ const app = express();
 let count = 0;
 
 app.get('/health', async (_req, res) => {
-  if (process.env.SLOW_MODE === 'true') {
-    await new Promise((r) => setTimeout(r, 3000));
-  }
+  await new Promise((r) => setTimeout(r, 3000));
 
   res.status(200).json({ status: 'ok - Application reached' });
 });
@@ -39,4 +37,3 @@ app.get('/sum', (_req, res) => {
 
 
 export {app, count};
-//
